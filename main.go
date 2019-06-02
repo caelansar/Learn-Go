9ca9eaf70d40ac4a6b7b6c7da18dd1886fe4ea56
caelansar <819711623@qq.com>
2019-06-02 13:37:07 +0800

    change

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"github.com/gin-gonic/gin"
+	"webapp/controller"
+	"webapp/middleware"
 )
 
 var buildstamp = ""
@@ -14,22 +15,24 @@ type H struct {
 }
 
 func main() {
-	//route := gin.Default()
-	//v1 := route.Group("/v1")
-	//{
-	//	v1.GET("/test", controller.Test)
-	//	v1.GET("/hello", controller.Hello)
-	//	userRouter := v1.Group("/user")
-	//	{
-	//		userRouter.POST("/register", controller.UserRegister)
-	//	}
-	//}
-	//route.Run()
-	args := os.Args
-	if len(args) == 2 && (args[1] == "--version" || args[1] == "-v") {
-		fmt.Printf("Go Version : %s\n", goversion)
-		fmt.Printf("UTC Build Time : %s\n", buildstamp)
-		return
+	route := gin.Default()
+	v1 := route.Group("/v1")
+	{
+		v1.GET("/test", controller.Test)
+		v1.GET("/hello", controller.Hello)
+		userRouter := v1.Group("/user")
+		{
+			userRouter.POST("/register", controller.UserRegister)
+			userRouter.POST("/login", controller.UserLogin)
+			userRouter.POST("/info", middleware.Auth, controller.UserInfo)
+		}
 	}
-	fmt.Println("hello world")
+	route.Run()
+	//args := os.Args
+	//if len(args) == 2 && (args[1] == "--version" || args[1] == "-v") {
+	//	fmt.Printf("Go Version : %s\n", goversion)
+	//	fmt.Printf("UTC Build Time : %s\n", buildstamp)
+	//	return
+	//}
+	//fmt.Println("hello world")
 }
